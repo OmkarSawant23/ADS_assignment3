112e5656b09c63aa953cd9e176ef5679a13b44c2
OmkarSawant23 <124716114+OmkarSawant23@users.noreply.github.com>
2024-01-18 18:48:03 +0530

    china arable land plot added

--- 22080770_ads3.py
+++ 22080770_ads3.py
@@ -57,7 +57,7 @@ def read_world_bank_csv(filename):
 def poly(x, a, b, c, d):
     """ Calulates polynominal"""
     
-    x = x - 1990
+    x = x - 1995
     f = a + b*x + c*x**2 + d*x**3
     
     return f
@@ -89,14 +89,14 @@ fert_data_yw, fert_data_cw = \
 ar_lnd_yw, ar_lnd_cw = read_world_bank_csv("API_AG.LND.ARBL.ZS_DS2_en_csv_v2_6302821.csv")
 cr_lnd_yw, cr_lnd_cw = read_world_bank_csv("API_AG.YLD.CREL.KG_DS2_en_csv_v2_6299928.csv")
 
-#print(ar_lnd_cw[1995])
+print(ar_lnd_yw["China"])
 ###############Clustering   ######################################
 ar_cluster =  pd.DataFrame()
 ar_cluster_1 = pd.DataFrame()
 ar_cluster_1["fert"] = fert_data_cw.loc[:,1995]
 ar_cluster_1["arable"] = cr_lnd_cw.loc[:,1995]
 ar_cluster_1["fert_2000"] = fert_data_cw.loc[:,2020]
-ar_cluster_1["arable_2000"] = cr_lnd_cw.loc[:,2020]
+ar_cluster_1["cereal_2000"] = cr_lnd_cw.loc[:,2020]
 ar_cluster_1 = ar_cluster_1.dropna()
 
 df_norm, df_min, df_max = ct.scaler(ar_cluster_1)
@@ -122,14 +122,13 @@ ykmeans = cen[:, 1]
 x1 = ar_cluster_1["fert"]
 y1 = ar_cluster_1["arable"]
 x2 = ar_cluster_1["fert_2000"]
-y2 = ar_cluster_1["arable_2000"]
+y2 = ar_cluster_1["cereal_2000"]
 plt.figure(figsize=(8.0, 8.0))
 # plot data with kmeans cluster number
 cm = plt.colormaps["Paired"]
 fig, c_f = plt.subplots(1, 2, figsize=(12, 5)) 
 scatter1 = c_f[0].scatter(x1, y1, 10, labels, marker="o", cmap=cm)
 c_f[0].scatter(xkmeans, ykmeans, 45, "k", marker="d")
-c_f[0].scatter(xkmeans, ykmeans, 45, "k", marker="+")
 c_f[0].set_xlabel("Fertilizer(kg)")
 c_f[0].set_ylabel("Cereal")
 c_f[0].set_title("Cereal yield vs fertilizer usage (1995)")
@@ -137,7 +136,6 @@ c_f[0].set_ylim(0, 30000)
 c_f[0].grid(True)
 scatter2 = c_f[1].scatter(x2, y2, s=10, c=labels, marker="o", cmap=cm)
 c_f[1].scatter(xkmeans, ykmeans, 45, "k", marker="d")
-c_f[1].scatter(xkmeans, ykmeans, 45, "k", marker="+")
 c_f[1].set_xlabel("Fertilizer(kg)")
 c_f[1].set_ylabel("Cereal")
 c_f[1].set_title("Cereal yield vs fertilizer usage (2022)")
@@ -146,6 +144,7 @@ c_f[1].grid(True)
 
 #####################fitting############################
 ar_cluster["fert_k"] = fert_data_yw["China"]
+ar_cluster["arable_k"] = ar_lnd_yw["China"]
 ar_cluster = ar_cluster.dropna()
 plt.figure()
 ar_cluster["Year"] = ar_cluster.index
@@ -155,6 +154,16 @@ param, covar = opt.curve_fit(poly, ar_cluster["Year"], ar_cluster["fert_k"])
 ar_cluster["fit"] = poly(ar_cluster["Year"], *param)
 
 ar_cluster.plot("Year", ["fert_k", "fit"])
+plt.title("China fertilizer consumption(kg per hectare of arable land)")
+plt.xlabel("Year")
+plt.ylabel("Fertilizer consumption(kg)")
+print(ar_cluster["arable_k"])
+param_1, covar_1 = opt.curve_fit(poly, ar_cluster["Year"], ar_cluster["arable_k"])
+ar_cluster["fit"] = poly(ar_cluster["Year"], *param_1)
+ar_cluster.plot("Year", ["arable_k", "fit"])
+plt.title("China arabel land %")
+plt.xlabel("Year")
+plt.ylabel("arabel land %")
 
 
 ############Forcast###############
@@ -172,7 +181,22 @@ plt.plot(year, forecast, label="forecast")
 plt.title("China fertilizer consumption(kg per hectare of arable land)")
 plt.xlabel("Year")
 plt.ylabel("Fertilizer consumption(kg)")
+plt.fill_between(year, low, up, color="yellow", alpha=0.7)
 
+year = np.arange(1995, 2025)
+forecast = poly(year, *param_1)
+sigma = err.error_prop(year, poly, param_1, covar_1)
+low = forecast - sigma
+up = forecast + sigma
+
+ar_cluster["fit"] = poly(ar_cluster["Year"], *param_1)
+
+plt.figure()
+plt.plot(ar_cluster["Year"], ar_cluster["arable_k"], label="arabel land %")
+plt.plot(year, forecast, label="forecast")
+plt.title("China arabel land %")
+plt.xlabel("Year")
+plt.ylabel("arabel land %")
 # plot uncertainty range
 plt.fill_between(year, low, up, color="yellow", alpha=0.7)
 
